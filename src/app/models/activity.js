13f7fe539654a1d1b70a6f05d6398de56f85ccb3
sungtaek <leesungtaek@gmail.com>
2019-02-26 10:23:09 +0900

    re-structure

--- src/app/models/activity.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-
-const activitySchema = new Schema({
-	_id: String
-}, { collection: 'activities'});
-
-module.exports = mongoose.model('activity', activitySchema);
